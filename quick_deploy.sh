be9a84744ef6fedab067abec28639ee15bd943c2
Jack Collins <jackcollins1434@gmail.com>
2023-04-13 23:13:54 +0100

    Automated push from CLI

--- quick_deploy.sh
+++ quick_deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+cd /home/ophois/jackcollins.me.uk
+
+rm -rf /home/ophois/jackcollins.me.uk/public/* && hugo
+
 git add *
 git commit -m "Automated push from CLI"
 git push -f origin master
