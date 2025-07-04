78223e994e28608f45deebdb494ef478eb8c4e9c
Jack Collins <44711059+0phoi5@users.noreply.github.com>
2025-07-04 14:33:46 +0100

    Delete quick_deploy.sh

--- quick_deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-cd /home/ophois/jackcollins.me.uk
-
-rm -rf /home/ophois/jackcollins.me.uk/public/* && hugo
-
-git add *
-git commit -m "Automated push from CLI"
-git push -f origin master
